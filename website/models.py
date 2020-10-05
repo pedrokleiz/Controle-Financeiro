a15577d9b42d5b2d55de110454ee7ceaf626a6ab
Pedro Kleiz <pkleiz@hotmail.com>
2020-10-05 03:52:11 -0300

    banco criado e tabelas

---
+++ website/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import datetime
+
+
+class Deposit(models.Model):
+    from django.utils import timezone
+    deposit_name = models.CharField(max_length=50)
+    deposit_value = models.DecimalField(
+        blank=False, null=False, max_digits=20, decimal_places=2)
+    deposit_date = models.DateTimeField(auto_now=True)
+
+
+class Withdrawal(models.Model):
+    withdrawal_name = models.CharField(max_length=50)
+    withdrawal_value = models.DecimalField(
+        blank=False, null=False, max_digits=20, decimal_places=2)
+    withdrawal_date = models.DateTimeField(auto_now=True)
